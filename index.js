f7b6280aa8a2ce27054a650b50ad44f709c1b9b8
Daria Burdyga <Daria.Burdyga@t-systems.com>
2021-09-02 18:25:10 +0300

    Add function playerLevel

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ function checkGuess() { // Проверить значение
     allertMessage.textContent = 'Вы угадали!'; // Моделирование сообщения о правильно угаданном числе
     allertMessage.style.color = 'green';
     value.textContent = '';
+    playerLevel();
     setGameOver();
   } else if (attemptsCount === 10) {
     allertMessage.textContent = 'Игра закончена'; // Моделирование сообщения об окончании игры
     value.textContent = '';
+    playerLevel();
     setGameOver();
   } else {
     allertMessage.textContent = 'Ошибка!'; // Моделирование сообщения о неверной попытке ввода не числа
@@ -38,6 +40,20 @@ function checkGuess() { // Проверить значение
 
   attemptsCount++;
   guessField.value = '';
+}
+
+function playerLevel() {
+  if(attemptsCount < 1) {
+    allertMessage.textContent = 'Ваш уровень: Чемпион';
+  } else if(attemptsCount < 2) {
+    allertMessage.textContent = 'Ваш уровень: Эксперт';
+  } else if(attemptsCount < 3) {
+    allertMessage.textContent = 'Ваш уровень: Профи';
+  } else if(attemptsCount < 4) {
+    allertMessage.textContent = 'Ваш уровень: Любитель';
+  } else if(attemptsCount > 8) {
+    allertMessage.textContent = 'Ваш уровень: Новичок';
+  }
 }
 
 function setGameOver() { // Начать новую игру
